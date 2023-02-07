52c425f764df7623574feef9c272e359595b28e2
iixin <s6777v@gmail.com>
2023-02-07 23:33:25 +0800

    v1.0

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use num::{bigint::{BigInt, ToBigInt}, Zero, One, ToPrimitive};
+
+trait Digit {
+    fn is_string_digit(&self) -> bool;
+    fn str_to_bigint(&self) -> Option<BigInt>;
+}
+
+impl Digit for String {
+    fn is_string_digit(&self) -> bool {
+        if !self[..].is_ascii() {
+            return false;
+        }
+        for item in self.as_bytes().iter() {
+            if !item.is_ascii_digit() {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fn str_to_bigint(&self) -> Option<BigInt> {
+        if !self.is_string_digit() {
+            return None;
+        }
+
+        let mut result = BigInt::zero();
+        let ten: BigInt = BigInt::one() * 10;
+        let len = self.len().to_u32().unwrap();
+        for (i, &item) in self.as_bytes().iter().enumerate() {
+            let x = (item - b'0').to_i32().unwrap();
+            let x = x.to_bigint().unwrap();
+
+            result = result + x * (&ten.pow(len - 1 - i.to_u32().unwrap()));
+        }
+        Some(result)
+    }
+}
+
+trait BigSqrt {
+    fn big_sqrt(&self,len:u32) -> Option<String>;
+}
+
+impl BigSqrt for BigInt {
+    fn big_sqrt(&self,len:u32) -> Option<String> {
+        if self < &BigInt::zero() {
+            return None;
+        }
+        let mut result = self.sqrt().to_string();
+        let index_of_point =result.to_string().len();
+        let ten: BigInt = BigInt::one() * 10;
+        for j in 1..len+1 {
+            let mut tirl = 0;
+
+            for i in 0..10 {
+                // let m = result.str_to_bigint().unwrap() + i;
+                let mm1 = result.str_to_bigint().unwrap()*10.to_bigint().unwrap() + i;
+                let mm=&mm1*&mm1;
+                let mx=&mm/ten.pow(2*j);
+                // println!("{} mx:{}",&mm1,&mx);
+
+                if &mx >= self {
+                    tirl = i - 1;
+
+                    break;
+                }
+                tirl = 9;
+            }
+            result.push_str(&tirl.to_string());
+        }
+        result.insert(index_of_point, '.');
+        Some(result)
+    }
+    
+}
+
+fn main() {
+    let x="2".to_string().str_to_bigint().unwrap(); // 2
+    println!("{}", x.big_sqrt(100).unwrap());               // sqrt(2) 小数点后1000位
+}
